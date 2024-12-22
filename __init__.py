7e8ea17d4a064f5d92229e2ae4116e52296020b3
Maksim <132957977+Maxim-Dey@users.noreply.github.com>
2024-12-22 07:24:53 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .MS_Time_Measure_Node import MS_Time_Measure_Node
+from .nodes.MS_Time_Measure_Node import MS_Time_Measure_Node
 NODE_CLASS_MAPPINGS = {"MS Time Measure Node": MS_Time_Measure_Node}
 __all__ = ["NODE_CLASS_MAPPINGS"]
